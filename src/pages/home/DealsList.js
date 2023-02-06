a8fe80f6ed698aaebe8fb9659819a7b1fc65c559
Zed Chamaa <sales@nineprovince.com>
2023-02-06 16:19:33 +0100

    Create the deals list component

---
+++ src/pages/home/DealsList.js
@@ -0,0 +1,24 @@
+import { useFirestore } from '../../hooks/useFirestore';
+
+// styles
+import styles from './Home.module.css';
+
+export default function DealsList({ deals }) {
+  const { deleteDocument } = useFirestore('deals');
+
+  return (
+    <ul className={styles.deals}>
+      {deals.map((deal) => (
+        <li key={deal.id}>
+          <p className={styles.name}>{deal.name}</p>
+          <p className={styles.name}>{deal.client}</p>
+          <p className={styles.amount}>{deal.amount}</p>
+          <p className={styles.amount}>{deal.stage}</p>
+          <p className={styles.amount}>{deal.status}</p>
+          <p className={styles.amount}>{deal.created}</p>
+          <button onClick={() => deleteDocument(deal.id)}>x</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
