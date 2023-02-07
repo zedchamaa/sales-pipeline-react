17daf6182f7c1a7efa1d4d2b698da54b919f5a60
Zed Chamaa <sales@nineprovince.com>
2023-02-07 11:55:59 +0100

    Create the announcement component

---
+++ src/components/Announcement.js
@@ -0,0 +1,13 @@
+// styles
+import styles from './Announcement.module.css';
+
+export default function Announcement({ children, title }) {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['title']}>
+        <strong>{title}</strong>
+      </div>
+      <div className={styles['children']}>{children}</div>
+    </div>
+  );
+}
