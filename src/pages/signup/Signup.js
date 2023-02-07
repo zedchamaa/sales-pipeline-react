d04089f85fbb98adbe4ae4389e623560ba9f1b61
Zed Chamaa <sales@nineprovince.com>
2023-02-07 10:51:10 +0100

    Alert user if passwords don't match on signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,50 +1,106 @@
-import { useState } from 'react'
-import { useSignup } from '../../hooks/useSignup'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSignup } from '../../hooks/useSignup';
 
 // styles
-import styles from './Signup.module.css'
+import styles from './Signup.module.css';
 
 export default function Signup() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [displayName, setDisplayName] = useState('')
-  const { signup, isPending, error } = useSignup()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+
+  const { signup, isPending, error } = useSignup();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    signup(email, password, displayName)
-  }
+    e.preventDefault();
+
+    // check if passwords match and display alert if they don't
+    if (password !== confirmPassword) {
+      setShowAlert(true);
+      return;
+    } else {
+      setShowAlert(false);
+    }
+
+    signup(email, password, displayName);
+  };
 
   return (
-    <form onSubmit={handleSubmit} className={styles['signup-form']}>
-      <h2>sign up</h2>
-      <label>
-        <span>email:</span>
-        <input 
-          type="email" 
-          onChange={(e) => setEmail(e.target.value)} 
-          value={email}
-        />
-      </label>
-      <label>
-        <span>password:</span>
-        <input 
-          type="password" 
-          onChange={(e) => setPassword(e.target.value)} 
-          value={password} 
-        />
-      </label>
-      <label>
-        <span>display name:</span>
-        <input 
-          type="text" 
-          onChange={(e) => setDisplayName(e.target.value)}
-          value={displayName}
-        />
-      </label>
-      { !isPending && <button className="btn">sign up</button> }
-      { isPending && <button className="btn" disabled>loading</button> }
-      { error && <p>{error}</p> }
+    <form
+      onSubmit={handleSubmit}
+      className={styles['form-container']}
+    >
+      <div className={styles['top-container']}>
+        <h1>Create a new account</h1>
+        <p>Please enter your details.</p>
+      </div>
+
+      <div className={styles['middle-container']}>
+        <div className={styles['labels']}>
+          <label>
+            <span>Email</span>
+            <input
+              type='email'
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder='example@email.com'
+            />
+          </label>
+          <label>
+            <span>Password</span>
+            <input
+              type='password'
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder='Enter password'
+            />
+          </label>
+          <label>
+            <span>Confirm Password</span>
+            <input
+              type='password'
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              placeholder='Confirm password'
+            />
+          </label>
+          {showAlert && (
+            <div className='form-alert'>*Passwords do not match</div>
+          )}
+          <label>
+            <span>Display Name</span>
+            <input
+              type='text'
+              onChange={(e) => setDisplayName(e.target.value)}
+              value={displayName}
+              placeholder='Enter display name'
+            />
+          </label>
+        </div>
+        <div>
+          {!isPending && <button className={styles['btn']}>Register</button>}
+          {isPending && (
+            <button
+              className={styles['btn']}
+              disabled
+            >
+              Loading...
+            </button>
+          )}
+        </div>
+      </div>
+      <div className={styles['bottom-container']}>
+        <p>
+          Have an account?{' '}
+          <Link to='/login'>
+            <strong>Login</strong>
+          </Link>
+        </p>
+        {error && <div className='form-alert'>{error}</div>}
+      </div>
     </form>
-  )
+  );
 }
