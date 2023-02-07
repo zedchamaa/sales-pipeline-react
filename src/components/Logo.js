5f5d2c5fcd45209ace915f25e27f125b46f7c54f
Zed Chamaa <sales@nineprovince.com>
2023-02-07 15:51:15 +0100

    Create the logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+// pages & components
+import LogoIcon from './icons/LogoIcon';
+
+export default function Logo() {
+  return (
+    <div className='logo'>
+      <LogoIcon />
+      SALES PIPELINE
+    </div>
+  );
+}
