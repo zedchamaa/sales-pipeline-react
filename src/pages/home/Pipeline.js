a06e8d6f83e7108cac5526173e57519b3a3afac4
Zed Chamaa <sales@nineprovince.com>
2023-02-08 13:11:31 +0100

    Implement the layout of the pipeline page

--- src/pages/home/Pipeline.js
+++ src/pages/home/Pipeline.js
@@ -1,11 +1,31 @@
+// styles
+import styles from './Pipeline.module.css';
+
 // pages & components
 import NavMenu from '../../components/NavMenu';
+import Sidebar from '../../components/Sidebar';
+import Topbar from '../../components/Topbar';
+import Footer from '../../components/Footer';
 
 export default function Pipeline() {
   return (
-    <div>
-      <NavMenu />
-      <h1>Pipeline Page</h1>
+    <div className={styles['page-container']}>
+      <div className={styles['sidebar']}>
+        <Sidebar />
+      </div>
+      <div>
+        <NavMenu />
+      </div>
+      <div className={styles['topbar']}>
+        <Topbar />
+      </div>
+
+      <div className={styles['main-content']}>
+        <h1>Pipeline Page</h1>
+      </div>
+      <div className={styles['footer']}>
+        <Footer />
+      </div>
     </div>
   );
 }
