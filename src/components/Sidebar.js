9807cbd5595778a9a71b2d6c0c82311a1cb15f02
Zed Chamaa <sales@nineprovince.com>
2023-02-08 14:48:21 +0100

    Add the logo and links to the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,34 @@
+import { NavLink } from 'react-router-dom';
+
 // styles
 import styles from './Sidebar.module.css';
 
+// pages & components
+import Logo from './Logo';
+import PipelineIcon from './icons/PipelineIcon';
+import DealsIcon from './icons/DealsIcon';
+
 export default function Sidebar() {
-  return <div className={styles['container']}>Sidebar</div>;
+  return (
+    <div className={styles['container']}>
+      <div className={styles['logo']}>
+        <Logo />
+      </div>
+      <div className={styles['menu-items']}>
+        <div className={styles['link-container']}>
+          <PipelineIcon />
+          <NavLink
+            exact
+            to='/'
+          >
+            Pipeline
+          </NavLink>
+        </div>
+        <div className={styles['link-container']}>
+          <DealsIcon />
+          <NavLink to='/deals'>Deals</NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
