70110fa3d771e6f224924a762cbc75df769b2832
Zed Chamaa <sales@nineprovince.com>
2023-02-09 11:47:50 +0100

    Add search component and button to top bar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,6 +1,18 @@
 // styles
 import styles from './Topbar.module.css';
 
+// pages & components
+import SearchDeals from './SearchDeals';
+import PlusIcon from '../components/icons/PlusIcon';
+
 export default function Topbar() {
-  return <div className={styles.container}>Topbar</div>;
+  return (
+    <div className={styles.container}>
+      <SearchDeals />
+      <div className={styles.buttonContainer}>
+        <PlusIcon />
+        <button>New Deals</button>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/icons/PlusIcon.js
@@ -0,0 +1,19 @@
+export default function PlusIcon() {
+  return (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M8.00004 3.33325V12.6666M3.33337 7.99992H12.6667'
+        stroke='#FCFCFD'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+}
