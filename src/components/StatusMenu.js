76919eea6511011cb04141b40fbc547a469e0eb3
Zed Chamaa <sales@nineprovince.com>
2023-02-10 15:05:23 +0100

    Apply styling to single labels in status menu

--- src/components/StatusMenu.js
+++ src/components/StatusMenu.js
@@ -2,15 +2,85 @@ import Select from 'react-select';
 
 export default function StatusMenu({ onChange }) {
   const statuses = [
-    { value: 'inprogress', label: 'In Progress' },
-    { value: 'good', label: 'Good' },
-    { value: 'stalled', label: 'Stalled' },
+    {
+      value: 'inprogress',
+      label: 'In Progress',
+      color: '#667085',
+      backgroundColor: '#F2F4F7',
+      width: '153px',
+      height: '31px',
+      borderRadius: '12px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '1.2rem',
+      fontWeight: '700',
+      lineHeight: '14.52px',
+    },
+    {
+      value: 'very-good',
+      label: 'Very Good',
+      color: '#12B76A',
+      backgroundColor: '#D1FADF',
+      width: '153px',
+      height: '31px',
+      borderRadius: '12px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '1.2rem',
+      fontWeight: '700',
+      lineHeight: '14.52px',
+    },
+    {
+      value: 'stalled',
+      label: 'Stalled',
+      color: '#F04438',
+      backgroundColor: '#FEE4E2',
+      width: '153px',
+      height: '31px',
+      borderRadius: '12px',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontSize: '1.2rem',
+      fontWeight: '700',
+      lineHeight: '14.52px',
+    },
   ];
+
+  const colorStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: '#FFFFFF' }),
+    option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+      return {
+        ...styles,
+        color: data.color,
+        backgroundColor: data.backgroundColor,
+      };
+    },
+    singleValue: (styles, { data }) => {
+      return {
+        ...styles,
+        color: data.color,
+        backgroundColor: data.backgroundColor,
+        width: data.width,
+        height: data.height,
+        borderRadius: data.borderRadius,
+        display: data.display,
+        alignItems: data.alignItems,
+        justifyContent: data.justifyContent,
+        fontSize: data.fontSize,
+        fontWeight: data.fontWeight,
+        lineHeight: data.lineHeight,
+      };
+    },
+  };
 
   return (
     <Select
       options={statuses}
       onChange={onChange}
+      styles={colorStyles}
     />
   );
 }
