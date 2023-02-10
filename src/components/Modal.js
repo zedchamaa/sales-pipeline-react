7c0d09d2f13edaa207c134280d8167b7182aa63a
Zed Chamaa <sales@nineprovince.com>
2023-02-10 09:51:46 +0100

    Add the title prop to the modal component

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,11 +4,12 @@ import styles from './Modal.module.css';
 // pages & components
 import CloseIcon from './icons/CloseIcon';
 
-export default function Modal({ children, onClick }) {
+export default function Modal({ children, onClick, title }) {
   return (
     <div className={styles.modalBackdrop}>
       <div className={styles.modal}>
         <div className={styles.iconContainer}>
+          <div className={styles.title}>{title}</div>
           <CloseIcon
             onClick={onClick}
             stroke={'black'}
