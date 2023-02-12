4b0343ca7c460262ede4ba3eed6ee8a22d0fdc2d
Zed Chamaa <sales@nineprovince.com>
2023-02-12 14:04:33 +0100

    Show each deal separately in its own card

--- src/pages/deals/DealsList.js
+++ src/pages/deals/DealsList.js
@@ -1,24 +1,83 @@
 import { useFirestore } from '../../hooks/useFirestore';
 
 // styles
-import styles from './Deals.module.css';
+import styles from './DealsList.module.css';
+
+// libraries
+import { formatNumber } from 'accounting';
+
+// components
+import TrashcanIcon from '../../components/icons/TrashcanIcon';
+import EditIcon from '../../components/icons/EditIcon';
 
 export default function DealsList({ deals }) {
   const { deleteDocument } = useFirestore('deals');
 
-  return (
-    <ul className={styles.deals}>
-      {deals.map((deal) => (
-        <li key={deal.id}>
-          <p className={styles.name}>{deal.name}</p>
-          <p className={styles.name}>{deal.client}</p>
-          <p className={styles.amount}>{deal.amount}</p>
-          <p className={styles.amount}>{deal.stage}</p>
-          <p className={styles.amount}>{deal.status}</p>
-          <p className={styles.amount}>{deal.created}</p>
-          <button onClick={() => deleteDocument(deal.id)}>x</button>
-        </li>
-      ))}
-    </ul>
-  );
+  return deals.map((deal) => (
+    <div className={styles.container}>
+      <div
+        className={styles.card}
+        key={deal.id}
+      >
+        <div className={styles.cardTop}>
+          <div className={styles.titles}>
+            <div className={styles.dealName}>{deal.name}</div>
+            <div className={styles.clientName}>{deal.client}</div>
+          </div>
+          <div className={styles.icons}>
+            <div>
+              <TrashcanIcon onClick={() => deleteDocument(deal.id)} />
+            </div>
+            <div>
+              <EditIcon />
+            </div>
+          </div>
+        </div>
+        <div className={styles.cardBottom}>
+          <div className={styles.content}>
+            <div className={styles.colOne}>
+              <span className={styles.dealInfo}>Deal Value</span>
+            </div>
+            <div className={styles.colTwo}>
+              <span className={styles.dealValue}>
+                {formatNumber(deal.amount)}
+              </span>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.colOne}>
+              <span className={styles.dealInfo}>Deal Status</span>
+            </div>
+            <div className={styles.colTwo}>
+              <div
+                className={
+                  (deal.status === 'In Progress' ? styles.inProgress : '') ||
+                  (deal.status === 'Very Good' ? styles.veryGood : '') ||
+                  (deal.status === 'Stalled' ? styles.stalled : '')
+                }
+              >
+                {deal.status}
+              </div>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.colOne}>
+              <span className={styles.dealInfo}>Deal Stage</span>
+            </div>
+            <div className={styles.colTwo}>
+              <span className={styles.dealStage}>{deal.stage}</span>
+            </div>
+          </div>
+          <div className={styles.content}>
+            <div className={styles.colOne}>
+              <span className={styles.dealInfo}>Date Created</span>
+            </div>
+            <div className={styles.colTwo}>
+              <span className={styles.dealCreatedDate}>{deal.created}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
 }
