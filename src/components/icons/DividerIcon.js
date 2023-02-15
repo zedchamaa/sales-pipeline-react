c5622270acd387d2e94e12c68158e8f009dda596
Zed Chamaa <sales@nineprovince.com>
2023-02-15 11:19:44 +0100

    Create the divider icon

---
+++ src/components/icons/DividerIcon.js
@@ -0,0 +1,17 @@
+export default function DividerIcon() {
+  return (
+    <svg
+      width='230'
+      height='1'
+      viewBox='0 0 230 1'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <rect
+        width='230'
+        height='1'
+        fill='#EAECF0'
+      />
+    </svg>
+  );
+}
