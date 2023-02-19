98f8c8175407b98cda1c5f7a9e43283204cee555
Zed Chamaa <sales@nineprovince.com>
2023-02-19 12:45:59 +0100

    Prevent iPhone from zooming in on `select` - v5

--- src/components/StagesMenu.js
+++ src/components/StagesMenu.js
@@ -2,12 +2,12 @@ import Select from 'react-select';
 
 export default function StagesMenu({ onChange, stage }) {
   const stages = [
-    { value: 'Qualified', label: 'Qualified', fontSize: '1.7rem' },
-    { value: 'Demo', label: 'Demo', fontSize: '1.7rem' },
-    { value: 'Proposal', label: 'Proposal', fontSize: '1.7rem' },
-    { value: 'Negotiations', label: 'Negotiations', fontSize: '1.7rem' },
-    { value: 'Won', label: 'Won', fontSize: '1.7rem' },
-    { value: 'Lost', label: 'Lost', fontSize: '1.7rem' },
+    { value: 'Qualified', label: 'Qualified' },
+    { value: 'Demo', label: 'Demo' },
+    { value: 'Proposal', label: 'Proposal' },
+    { value: 'Negotiations', label: 'Negotiations' },
+    { value: 'Won', label: 'Won' },
+    { value: 'Lost', label: 'Lost' },
   ];
 
   const menuContainer = document.body; // or any other container with a max-width set
--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 
+// libraries
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
+      <HelmetProvider>
+        <Helmet>
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no'
+          />
+        </Helmet>
+      </HelmetProvider>
       <App />
     </AuthContextProvider>
   </React.StrictMode>,
