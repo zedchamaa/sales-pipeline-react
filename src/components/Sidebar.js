b109af1000455caab201552373f176b59f937e17
Zed Chamaa <sales@nineprovince.com>
2023-02-24 11:42:02 +0100

    Highlight the link container of the selected page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 // styles
 import styles from './Sidebar.module.css';
@@ -8,7 +9,23 @@ import Logo from './Logo';
 import PipelineIcon from './icons/PipelineIcon';
 import DealsIcon from './icons/DealsIcon';
 
+let activeClass = 'link-container-active';
+let defaultClass = 'link-container';
+
 export default function Sidebar() {
+  const currentPage = window.location.pathname;
+  const [classOne, setClassOne] = useState(defaultClass);
+  const [classTwo, setClassTwo] = useState(defaultClass);
+
+  // change link container class name based on current page
+  useEffect(() => {
+    if (currentPage === '/') {
+      setClassOne(activeClass);
+    } else if (currentPage === '/deals') {
+      setClassTwo(activeClass);
+    }
+  }, [currentPage]);
+
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
@@ -18,11 +35,11 @@ export default function Sidebar() {
         />
       </div>
       <div className={styles.menuItems}>
-        <div className={styles.linkContainer}>
+        <div className={classOne}>
           <PipelineIcon />
           <NavLink to='/'>Pipeline</NavLink>
         </div>
-        <div className={styles.linkContainer}>
+        <div className={classTwo}>
           <DealsIcon />
           <NavLink to='/deals'>Deals</NavLink>
         </div>
